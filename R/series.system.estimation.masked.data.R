4b81637d08f83f8f2816a2a69e1fa9aa5b92ef71
Alex Towell <queelius@gmail.com>
2022-05-21 03:44:05 -0500

    Update series.system.estimation.masked.data.R

--- R/series.system.estimation.masked.data.R
+++ R/series.system.estimation.masked.data.R
@@ -5,18 +5,6 @@
 #' framework for estimating series system lifetime component parameters
 #' from masked data.
 #'
-#' @section data structures:
-#' 
-#' \code{md_estimate}
-#'
-#' @section MLE point estimators:
-#'
-#' \code{md_mle_exp_series_m0}
-#' \code{md_mle_exp_series_m1}
-#' \code{md_mle_exp_series_m0}
-#' \code{point.md_estimate}
-#'
-#'
 #' @docType package
 #' @name series.system.estimation.masked.data
 NULL
