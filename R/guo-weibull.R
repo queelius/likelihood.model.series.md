64cddf9b10983f0f2a9c6f11687bd3d6765660e3
queelius <queelius@gmail.com>
2022-05-21 04:21:35 -0500

    updates

---
+++ R/guo-weibull.R
@@ -0,0 +1,12 @@
+#' Masked data for a series system with a real world data set from Guo.
+#'
+#'
+#' @format A data frame with 1000 rows and 9 variables:
+#' \describe{
+#'   \item{t}{Real observable variable, system lifetime}
+#'   \item{x1}{Boolean observable variable, TRUE indicates node 1 is in candidate set}
+#'   \item{x2}{Boolean observable variable, TRUE indicates node 2 is in candidate set}
+#'   \item{x3}{Boolean observable variable, TRUE indicates node 3 is in candidate set}
+#' }
+#' @source \url{?}
+"guo-weibull-tab1"
